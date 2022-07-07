df5c8d98d52d114dd68942e72f2c6b6571f80343
PhlipTung <philip.dongfei@gmail.com>
2022-07-07 10:14:37 +0800

    ch3 fin

---
+++ Chapter03/README.md
@@ -0,0 +1,3 @@
+#Chapter03
+[read linux kernel code](https://elixir.bootlin.com/linux/latest/source)
+
---
+++ Chapter03/ch3/os/src/main.rs
@@ -0,0 +1,56 @@
+
+//#![deny(missing_docs)]
+//#![deny(warnings)]
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use core::arch::global_asm;
+
+
+#[cfg(not(any(feature = "board_k210")))]
+#[path = "boards/qemu.rs"]
+mod board;
+
+#[macro_use]
+mod console;
+mod config;
+mod lang_items;
+mod loader;
+mod sbi;
+mod sync;
+pub mod syscall;
+pub mod task;
+mod timer;
+pub mod trap;
+
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+/// clear BSS segment
+fn clear_bss() {
+    // get sbss,ebss fun interface 
+    // TODO: call sleep fun
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8, 
+            ebss as usize - sbss as usize).fill(0);
+    }
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("\x1b[31m[Kernel] Hello, World!\x1b[0m");
+    trap::init();
+    loader::load_apps();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
+    task::run_first_task();
+    panic!("Unreachable in rust_main!");
+}
